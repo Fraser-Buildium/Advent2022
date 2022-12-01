ae94e6d487bdee1b2ddcc6e9cbf16dc4fceab676
Michael Fraser <michael.fraser@buildium.com>
2022-12-01 18:08:08 -0500

    Day 1 completed.

---
+++ Advent2022/Day1.cs
@@ -0,0 +1,32 @@
+﻿namespace Advent2022;
+
+public class Day1
+{
+    public static void Execute()
+    {
+        var lines = File.ReadAllLines("input1.txt");
+        var elfCalories = new SortedSet<int>();
+        var currentSum = 0;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                elfCalories.Add(currentSum);
+                currentSum = 0;
+                continue;
+            }
+
+            if (int.TryParse(line, out var currentCalories))
+            {
+                currentSum += currentCalories;
+            }
+        }
+
+        // Part 1
+        Console.Out.WriteLine(elfCalories.Last());
+
+        // Part 2
+        Console.Out.WriteLine(elfCalories.TakeLast(3).Sum());
+    }
+}
---
+++ Advent2022/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Advent2022;
+
+Day1.Execute();
