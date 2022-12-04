3d7ec91a1447462237317d6b080f934baf12e39d
Michael Fraser <michael.fraser@buildium.com>
2022-12-04 16:37:17 -0500

    Cleaning up Day 4 code.

--- Advent2022/Day4.cs
+++ Advent2022/Day4.cs
@@ -8,13 +8,7 @@ public static class Day4
         var sum = 0;
         foreach (var line in lines)
         {
-            var pair = line.Split(",");
-            var range1 = pair[0].Split("-");
-            var range2 = pair[1].Split("-");
-            var start1 = int.Parse(range1[0]);
-            var end1 = int.Parse(range1[1]);
-            var start2 = int.Parse(range2[0]);
-            var end2 = int.Parse(range2[1]);
+            var (start1, end1, start2, end2) = Parse(line);
             if (start1 <= start2 && end2 <= end1)
             {
                 Console.Out.WriteLine($"Range 2 fully contained: {line}");
@@ -26,7 +20,7 @@ public static class Day4
                 sum++;
             }
         }
-        Console.Out.WriteLine($"Total 2: {sum}");
+        Console.Out.WriteLine($"Total 1: {sum}");
     }
     
     public static void Execute2()
@@ -35,23 +29,33 @@ public static class Day4
         var sum = 0;
         foreach (var line in lines)
         {
-            var pair = line.Split(",");
-            var range1 = pair[0].Split("-");
-            var range2 = pair[1].Split("-");
-            var start1 = int.Parse(range1[0]);
-            var end1 = int.Parse(range1[1]);
-            var start2 = int.Parse(range2[0]);
-            var end2 = int.Parse(range2[1]);
+            var (start1, end1, start2, end2) = Parse(line);
             var range1Numbers = Enumerable.Range(start1, end1 - start1 + 1).ToHashSet();
             var range2Numbers = Enumerable.Range(start2, end2 - start2 + 1).ToHashSet();
-            var intersection = range1Numbers.Intersect(range2Numbers).ToList();
-            if (intersection.Any())
+            var intersection = range1Numbers
+                .Intersect(range2Numbers)
+                .ToList();
+
+            if (!intersection.Any())
             {
-                Console.Out.WriteLine($"{line} intersection {intersection.First()}-{intersection.Last()}");
-                sum++;
+                continue;
             }
+            
+            var intersectionStart = intersection.First();
+            var intersectionEnd = intersection.Last();
+            Console.Out.WriteLine($"{line} intersection {intersectionStart}-{intersectionEnd}");
+            sum++;
         }
         
         Console.Out.WriteLine($"Total 2: {sum}");
+    }
+
+    private static (int start1, int end1, int start2, int end2) Parse(string line)
+    {
+        var parsedArray = line.Split(",")
+            .SelectMany(pair => pair.Split("-"))
+            .Select(int.Parse)
+            .ToArray();
+        return (parsedArray[0], parsedArray[1], parsedArray[2], parsedArray[3]);
     }
 }
